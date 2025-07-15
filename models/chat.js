bd28296e108f551bf8257732b5a209d9f86a60f4
apagrouppro <uefamobile1@gmail.com>
2025-07-15 03:37:21 +0300

    Delete models directory

--- models/chat.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatSchema = new mongoose.Schema({
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-});
-
-module.exports = mongoose.model('Chat', ChatSchema);
